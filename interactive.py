7364d1135e300bbe5fb9ac60aad19296952b86a8
sigvaldm <sigvaldm@fys.uio.no>
2020-01-13 15:36:32 +0100

    Initial saving of data from plot

--- interactive.py
+++ interactive.py
@@ -9,3 +9,15 @@ ax.set_ylim(0,1)
 ax.set_aspect('equal')
 
 plt.show()
+
+particles = []
+for circ in ax.patches:
+    pos = np.array(circ.center)
+    if circ.arrow is not None:
+        length = np.array(circ.arrow.length)
+    else:
+        length = 0
+    color = circ.get_fc()
+    particles.append(Particle(pos, length, color))
+
+print(len(particles))
