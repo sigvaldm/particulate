d3c46d7a814bf6135bd8dae7c24d3ef9c5585a9d
sigvaldm <sigvaldm@fys.uio.no>
2020-01-13 13:27:38 +0100

    Can edit and add particles

--- interactive.py
+++ interactive.py
@@ -2,15 +2,10 @@ from particulate import *
 
 fig, ax = plt.subplots()
 
-state=0 # 0: place particle, 1: place velocity vector
-
 interact = Interactive(fig)
 
 ax.set_xlim(0,1)
 ax.set_ylim(0,1)
 ax.set_aspect('equal')
-
-# circ = plt.Circle([0.5, 0.5], 0.01)
-# ax.add_patch(circ)
 
 plt.show()
--- particulate.py
+++ particulate.py
@@ -35,8 +35,9 @@ def circle(ax, base, color='C0', size=0.01):
         'radius': size,
         'picker': True,
     }
-    circ = plt.Circle(base, **circlestyle)
-    return ax.add_patch(circ)
+    circle = plt.Circle(base, **circlestyle)
+    circle.arrow = None
+    return ax.add_patch(circle)
 
 def arrow(ax, circle, length=None, tip=None, color='C0', size=0.01):
 
@@ -58,6 +59,8 @@ def arrow(ax, circle, length=None, tip=None, color='C0', size=0.01):
     }
     arrow = ax.arrow(base[0], base[1], length[0], length[1], **arrowstyle)
     arrow.circle = circle
+    arrow.length = length
+    circle.arrow = arrow
     return arrow
 
 class Particle(object):
@@ -70,39 +73,60 @@ class Interactive(object):
 
     def __init__(self, fig, ax=None):
 
-        self.state = 0 # 0: place particle, 1: place velocity vector
+        self.state = 2 # 0: place particle, 1: place velocity vector
         self.fig = fig
         self.ax = fig.axes[0] if ax == None else ax
 
-        self.current_artist = circle(self.ax, [0.5, 0.5])
+        # self.current_artist = circle(self.ax, [0.5, 0.5])
+        circ = circle(self.ax, [0.5, 0.5])
+        arrow(self.ax, circ, [0.1,0])
+        self.current_artist = None
+        self.just_picked = False
 
         fig.canvas.mpl_connect('button_press_event', self.onClick)
         fig.canvas.mpl_connect('motion_notify_event', self.onMove)
         fig.canvas.mpl_connect('pick_event', self.onPick)
 
     def onClick(self, event):
-        self.state = (self.state+1)%3
+        pos = [event.xdata, event.ydata]
 
-        if self.state==0:
-            self.current_artist = circle(self.ax, [0.5, 0.5])
-        elif self.state==1:
-            self.current_artist = arrow(self.ax, self.current_artist, tip=[event.xdata, event.ydata])
-        elif self.state==2:
+        if self.just_picked:
+            self.just_picked = False
+            return
+
+        if isinstance(self.current_artist, mpl.patches.Circle):
+            if self.current_artist.arrow is None:
+                self.current_artist = arrow(self.ax, self.current_artist, tip=pos)
+            else:
+                self.current_artist = None
+        elif isinstance(self.current_artist, mpl.patches.FancyArrow):
             self.current_artist = None
+        elif event.dblclick:
+            self.current_artist = circle(self.ax, pos)
 
     def onMove(self, event):
+        pos = [event.xdata, event.ydata]
+
         if isinstance(self.current_artist, mpl.patches.Circle):
+            # Currently holding and moving a circle, perhaps with an arrow
+            length = None
+            if self.current_artist.arrow is not None:
+                length = self.current_artist.arrow.length
+                self.current_artist.arrow.remove()
             self.current_artist.remove()
-            self.current_artist = circle(self.ax, [event.xdata, event.ydata])
+            self.current_artist = circle(self.ax, pos)
+            if length is not None:
+                self.current_artist.arrow = arrow(self.ax, self.current_artist, length=length)
+
         elif isinstance(self.current_artist, mpl.patches.FancyArrow):
+            # Currently holding and moving an arrow
             self.current_artist.remove()
-            self.current_artist = arrow(self.ax, self.current_artist.circle, tip=[event.xdata, event.ydata])
+            self.current_artist = arrow(self.ax, self.current_artist.circle, tip=pos)
 
         self.fig.canvas.draw()
 
     def onPick(self, event):
-        if self.state==2:
-            if isinstance(event.artist, plt.Circle):
-                self.current_artist = event.artist
-            else:
-                self.current_artist = event.artist
+
+        if self.current_artist is None:
+            self.current_artist = event.artist
+            self.just_picked = True
